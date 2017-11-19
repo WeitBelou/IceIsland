63a43bfd1329046a9cdbe0d8918c6b2356820307
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-11-19 14:44:47 +0300

    Update logging format.

--- iceisland/log.py
+++ iceisland/log.py
@@ -4,11 +4,15 @@ log = logging.getLogger('ice_island')
 
 
 def _init_logger():
-    file = logging.FileHandler('ice_island.log')
+    formatter = logging.Formatter('[%(levelname)s] [%(asctime)s]  %(message)s')
+
+    file = logging.FileHandler('ice_island.log',)
     file.setLevel(logging.INFO)
+    file.setFormatter(formatter)
 
     console = logging.StreamHandler()
     console.setLevel(logging.INFO)
+    console.setFormatter(formatter)
 
     log.addHandler(console)
     log.addHandler(file)
