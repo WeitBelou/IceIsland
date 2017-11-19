bf9fb4be1849510dbf90560c84702a806351dcc0
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-11-19 20:52:07 +0300

    Fix layers order.

--- iceisland/meshes.py
+++ iceisland/meshes.py
@@ -9,8 +9,8 @@ def create_from_layers(size: float, layers: List[Layer]) -> Mesh:
     a = size / 2
     h = sum([layer.h for layer in layers])
 
-    n_x = n_y = 20
-    n_z = int(5 * h / min([layer.h for layer in layers]))
+    n_x = n_y = 10
+    n_z = 5 * n_x
 
     return BoxMesh(
         Point(-a, -a, 0), Point(a, a, h),
--- iceisland/settings.py
+++ iceisland/settings.py
@@ -6,15 +6,15 @@ g = 9.8 * METER / SECOND ** 2
 size = 1 * KILOMETER
 
 layers = [
-    # Bottom sediments
-    Layer(h=50 * METER, rho=1.5 * GRAM / CENTIMETER ** 3, young_modulus=0.016 * GPA, shear_modulus=0.0054 * GPA),
-
-    # First dense layer
-    Layer(h=300 * METER, rho=2.1 * GRAM / CENTIMETER ** 3, young_modulus=5.9 * GPA, shear_modulus=2.1 * GPA),
+    # Crystal foundation
+    Layer(h=1.0 * KILOMETER, rho=2.5 * GRAM / CENTIMETER ** 3, young_modulus=37.0 * GPA, shear_modulus=15.6 * GPA),
 
     # SECOND dense layer
     Layer(h=400 * METER, rho=2.5 * GRAM / CENTIMETER ** 3, young_modulus=12.0 * GPA, shear_modulus=4.2 * GPA),
 
-    # Crystal foundation
-    Layer(h=1.0 * KILOMETER, rho=2.5 * GRAM / CENTIMETER ** 3, young_modulus=37.0 * GPA, shear_modulus=15.6 * GPA),
+    # First dense layer
+    Layer(h=300 * METER, rho=2.1 * GRAM / CENTIMETER ** 3, young_modulus=5.9 * GPA, shear_modulus=2.1 * GPA),
+
+    # Bottom sediments
+    Layer(h=50 * METER, rho=1.5 * GRAM / CENTIMETER ** 3, young_modulus=0.016 * GPA, shear_modulus=0.0054 * GPA),
 ]
