36bfc755cd0d2efa470abe286534a63e79447ef0
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-11-19 14:17:21 +0300

    Remove duplicated entries.

--- iceisland/config.py
+++ iceisland/config.py
@@ -1,43 +1,6 @@
 from typing import List
 
 
-class Mesh:
-    def __init__(self, radius: float):
-        self._radius = radius
-
-    @property
-    def radius(self):
-        return self._radius
-
-    def __str__(self) -> str:
-        return '<Mesh radius={radius}>'.format(radius=self.radius)
-
-
-class Material:
-    def __init__(self, density: float, young_modulus: float, shear_modulus: float):
-        self._density = density
-
-        self._young_modulus = young_modulus
-        self._shear_modulus = shear_modulus
-
-    @property
-    def density(self):
-        return self._density
-
-    @property
-    def young_modulus(self):
-        return self._young_modulus
-
-    @property
-    def shear_modulus(self):
-        return self._shear_modulus
-
-    def __str__(self) -> str:
-        return '<Material density={density} young_modulus={young_modulus} shear_modulus={shear_modulus}>'.format(
-            density=self.density, young_modulus=self.young_modulus, shear_modulus=self.shear_modulus
-        )
-
-
 class Layer:
     """"""
 
