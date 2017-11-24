281c48a84047c2797b40bec7f063b1fa3ca12d08
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-11-24 19:07:06 +0300

    Add script to build meshes in fenics format.

--- build_meshes.sh
+++ build_meshes.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-docker run --rm \
+BASENAME=geometry
+
+cd meshes
+
+for m in $(ls); do
+    cd ${m}
+
+    gmsh -3 ${BASENAME}.geo
+
+    docker run --rm \
     -v $(pwd):/home/fenics/shared \
     -w /home/fenics/shared \
     --env HOST_UID=$(id -u) \
     --env HOST_GID=$(id -g) \
     --env PYTHONPATH=/home/fenics/shared \
     quay.io/fenicsproject/stable \
-    "python3 -m iceisland"
+    "dolfin-convert ${BASENAME}.msh ${BASENAME}.xml"
+
+    cd ..
+done
